c1474cbd279954fac78e393b6e0ff4a612411477
gljubojevic <gljubojevic@users.noreply.github.com>
2020-10-08 21:39:55 +0200

    Fix bug with missing keys on line paths

--- src/editors/LineEdit.js
+++ src/editors/LineEdit.js
@@ -19,19 +19,19 @@ class LineEdit extends Component {
 		);
 	}
 
-	renderPath(p) {
+	renderPath(p, key) {
 		const d = "M " + p.p1.x + " " + p.p1.y + " C " + p.c1.x + " " + p.c1.y + " " + p.c2.x + " " + p.c2.y + " " + p.p2.x + " " + p.p2.y;
 		const markerStart = p.arrowStart ? "url(#arrowStart)" : "";
 		const markerEnd = p.arrowEnd ? "url(#arrowEnd)" : "";
 		return (
-			<path d={d} data-ref={p.id} markerStart={markerStart} markerEnd={markerEnd} />
+			<path key={key} d={d} data-ref={p.id} markerStart={markerStart} markerEnd={markerEnd} />
 		);
 	}
 
 	render() {
 		const l = this.props.line;
 		const className = (l.dashed ? 'line dashed pc' : 'line pc') + l.color;
-		const allPaths = l.paths().map(p => this.renderPath(p));
+		const allPaths = l.paths().map((p, index) => this.renderPath(p, index.toString()));
 		return (
 			<g className={className}>
 				{allPaths}
