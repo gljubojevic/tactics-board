ac02f150eea7bf6c9f0ba1a3042ac1dcd1ac1e6f
gljubojevic <gljubojevic@users.noreply.github.com>
2020-10-15 19:03:04 +0200

    Remove box properties

--- src/pitch/Box.js
+++ src/pitch/Box.js
@@ -1,45 +1,10 @@
 class Box {
 	constructor(id="", x=0, y=0, width=0, height=0){
-		this._id = id;
-		this._x = x;
-		this._y = y;
-		this._width = width;
-		this._height = height;
-	}
-
-    get id() {
-		return this._id;
-	}
-	set id(value) {
-		this._id = value;
-	}
-
-	get x() {
-		return this._x;
-	}
-	set x(value) {
-		this._x = value;
-	}
-
-	get y() {
-		return this._y;
-	}
-	set y(value) {
-		this._y = value;
-	}
-
-	get width() {
-		return this._width;
-	}
-	set width(value) {
-		this._width = value;
-	}
-
-	get height() {
-		return this._height;
-	}
-	set height(value) {
-		this._height = value;
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
 	}
 }
 
