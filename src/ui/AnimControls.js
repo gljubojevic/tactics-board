dcfe70c55eefceee2e30859069485d91aa7a4643
gljubojevic <gljubojevic@users.noreply.github.com>
2021-08-17 20:16:18 +0200

    Remove useless div tags from AnimControls

--- src/ui/AnimControls.js
+++ src/ui/AnimControls.js
@@ -12,69 +12,55 @@ import Tooltip from '@material-ui/core/Tooltip';
 
 class AnimControls extends Component {
 
-    render(){
-    
-        return (
-            <div >
-                <div  style={{float:"left"}}>
+	render(){
+		return (
+			<React.Fragment>
 
-                <Tooltip title="Previous Frame">
-                  <IconButton edge="end" color="inherit" aria-label="Previous Frame" onClick={this.props.keyFramePrevious} >
-                    <ArrowBackIcon/>
-                </IconButton>
-                </Tooltip>
-                
-                <Tooltip title="Current Frame">
-                <IconButton color="inherit" aria-label="Current Frame">
-                    {this.props.keyFrameCurrent}
-                </IconButton>
-                </Tooltip>
-                
-                <Tooltip title="Next Frame">
-                <IconButton edge="start" color="inherit" aria-label="Next Frame" onClick={this.props.keyFrameNext} >
-                    <ArrowForwardIcon />
-                </IconButton>
-                </Tooltip>
+				<Tooltip title="Previous Frame">
+					<IconButton edge="end" color="inherit" aria-label="Previous Frame" onClick={this.props.keyFramePrevious} >
+						<ArrowBackIcon/>
+					</IconButton>
+				</Tooltip>
+				<Tooltip title="Current Frame">
+					<IconButton color="inherit" aria-label="Current Frame">
+						{this.props.keyFrameCurrent}
+					</IconButton>
+				</Tooltip>
+				<Tooltip title="Next Frame">
+					<IconButton edge="start" color="inherit" aria-label="Next Frame" onClick={this.props.keyFrameNext} >
+						<ArrowForwardIcon />
+					</IconButton>
+				</Tooltip>
+				<Tooltip title="Add Frame">
+					<IconButton color="inherit" aria-label="Add Frame" onClick={this.props.keyFrameAdd} >
+						<AddIcon fontSize="large"></AddIcon>
+					</IconButton>
+				</Tooltip>
 
-                <Tooltip title="Add Frame">
-                <IconButton color="inherit" aria-label="Add Frame" onClick={this.props.keyFrameAdd} >
-                    <AddIcon  fontSize="large"></AddIcon>
-                </IconButton>
-                </Tooltip>
+				<Tooltip title="Play Animation">
+					<IconButton aria-label="Play Animation" color="inherit">
+						<PlayArrowIcon fontSize="large" />
+					</IconButton>
+				</Tooltip>
+				<Tooltip title="Pause Animation">
+					<IconButton aria-label="Pause Animation" color="inherit" >
+						<PauseIcon fontSize="large"/>
+					</IconButton>
+				</Tooltip>
+				<Tooltip title="Stop Animation">
+					<IconButton aria-label="Stop Animation" color="inherit" >
+						<StopIcon fontSize="large"/>
+					</IconButton>
+				</Tooltip>
+				<Tooltip title="Loop Animation">
+					<IconButton  aria-label="Loop Animation" color="inherit" >
+						<LoopIcon fontSize="large"/>
+					</IconButton>
+				</Tooltip>
 
-                </div>
-                
-                <div style={{float:'right'}}>
-
-                <Tooltip title="Play Animation">
-                <IconButton aria-label="Play Animation" color="inherit">
-                    <PlayArrowIcon fontSize="large" />
-                </IconButton>
-                </Tooltip>
-
-                <Tooltip title="Pause Animation">
-                <IconButton aria-label="Pause Animation" color="inherit" >
-                    <PauseIcon fontSize="large"/>
-                </IconButton>
-                </Tooltip>
-                
-                <Tooltip title="Stop Animation">
-                <IconButton aria-label="Stop Animation" color="inherit" >
-                    <StopIcon fontSize="large"/>
-                </IconButton>
-                </Tooltip>
-                
-                <Tooltip title="Loop Animation">
-                <IconButton  aria-label="Loop Animation" color="inherit" >
-                    <LoopIcon fontSize="large"/>
-                </IconButton>
-                </Tooltip>
-
-                </div>
-            </div>
-        );
-    }
-    
+			</React.Fragment>
+		);
+	}
 }
 
 AnimControls.defaultProps = {
