22958e8b7a9bde16f5ee9ac87003f322d73f28bf
gljubojevic <gljubojevic@users.noreply.github.com>
2021-10-19 21:41:13 +0200

    Change default ladder with looks better

--- src/pitch/Constants.js
+++ src/pitch/Constants.js
@@ -21,7 +21,7 @@ const ExtrasType = {
 const ExtrasDefaults = [
 	{ name:'Goal', width: 100, height: 300, viewBoxScale: 1 },
 	{ name:'Small Goal', width: 50, height: 150, viewBoxScale: 2 },
-	{ name:'Ladder', width: 150, height: 350, viewBoxScale: 1 },
+	{ name:'Ladder', width: 100, height: 350, viewBoxScale: 1 },
 	{ name:'Cone', width: 70, height: 85, viewBoxScale: 3 },
 	{ name:'Flag', width: 130, height: 280, viewBoxScale: 2 }
 ];
