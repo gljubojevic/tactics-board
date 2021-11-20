1f0ba84217e2fee762856288441e136e454d49d4
gljubojevic <gljubojevic@users.noreply.github.com>
2021-11-20 14:15:15 +0100

    Add firebase to application Will be used for: - savimg data - communication between clients

--- src/App.js
+++ src/App.js
@@ -11,6 +11,27 @@ import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
 import { v4 as uuidv4 } from 'uuid';
 import './App.css';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyDAb0oHSQCeihL0Op9agehWqYQh1-7E4xE",
+	authDomain: "tactics-board-1c644.firebaseapp.com",
+	projectId: "tactics-board-1c644",
+	storageBucket: "tactics-board-1c644.appspot.com",
+	messagingSenderId: "704589627362",
+	appId: "1:704589627362:web:b110cc834a5f5c19c543bb",
+	measurementId: "G-PDXB2VPRL8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 class App extends Component {
 	constructor(props) {
